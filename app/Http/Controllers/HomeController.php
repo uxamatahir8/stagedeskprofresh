ac9eb718bff03d9a1dfb8afa00e5a4ac58bf489c
usamatahirmicrorage <usama.saleem@microrage.com>
2025-10-20 02:32:03 +0500

    added fornt end webiste

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ class HomeController extends Controller
     //
     public function index()
     {
-        return view('website');
+        $title = 'Home - StageDesk Pro';
+        return view('website', compact('title'));
     }
 }
---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=`device-width`, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--=====TITLE=======-->
+    <title>{{ $title }}</title>
+
+    <!--=====FAV ICON=======-->
+    <link rel="shortcut icon" href="{{ asset('landing/images/logo/logo6.png') }}">
+
+    <!--=====CSS=======-->
+    <link rel="stylesheet" href="{{ asset('landing/css/plugins/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('landing/css/plugins/swiper.bundle.css') }}">
+    <link rel="stylesheet" href="{{ asset('landing/css/plugins/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('landing/css/plugins/mobile.css') }}">
+    <link rel="stylesheet" href="{{ asset('landing/css/plugins/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('landing/css/plugins/slick-slider.css') }}">
+    <link rel="stylesheet" href="{{ asset('landing/css/plugins/owlcarousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('landing/css/plugins/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('landing/css/typography.css') }}">
+    <link rel="stylesheet" href="{{ asset('landing/css/master.css') }}">
+
+    <!--=====JQUERY=======-->
+    <script src="{{ asset('landing/js/plugins/jquery-3-6-0.min.js') }}"></script>
+</head>
