92148fdab19f147744af8e2476e001ca9d8c14bd
usamatahirmicrorage <usama.saleem@microrage.com>
2025-10-28 17:59:32 +0500

    added models and migrations for comment system

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['user_id', 'blog_id', 'parent_id', 'content', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id')->with('user', 'replies');
+    }
 }
