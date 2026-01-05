af32906bb63321184a62bae2951126797875f727
usamatahir <uxamatahir8@gmail.com>
2026-01-06 01:28:25 +0500

    added bookings view

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -1,10 +1,35 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\BookingRequest;
+use Illuminate\Support\Facades\Auth;
 
 class BookingController extends Controller
 {
     //
+
+    public function index()
+    {
+        $title = 'Booking Requests';
+
+        $user_role = Auth::user()->role->role_key;
+
+        switch ($user_role) {
+            case 'master_admin':
+                $bookings = BookingRequest::all();
+                break;
+            case 'company_admin':
+                $bookings = BookingRequest::companyBookings()->get();
+                break;
+            case 'customer':
+                $bookings = BookingRequest::userBookings()->get();
+                break;
+            default:
+                $bookings = collect(); // empty collection for unauthorized roles
+                break;
+        }
+
+        return view('dashboard.pages.bookings.index', compact('title', 'bookings'));
+
+    }
 }
