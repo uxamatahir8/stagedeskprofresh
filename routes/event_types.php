08748cf52e4a5b60e9ef49f17af3b44fa28f8247
usamatahir <uxamatahir8@gmail.com>
2026-01-08 08:18:35 +0500

    completed event types crud

--- routes/event_types.php
+++ routes/event_types.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\EventTypeController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'role:master_admin'])->group(function () {
     Route::get('/event-types', [EventTypeController::class, 'index'])->name('event-types');
     //edit route
     Route::get('/event-type/edit/{id}', [EventTypeController::class, 'edit'])->name('event.edit');
