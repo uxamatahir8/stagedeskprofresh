355bf1f0bef3adce47e64f497b7718946217f181
usamatahir <uxamatahir8@gmail.com>
2026-01-08 10:05:58 +0500

    modify booking_requests table

--- database/migrations/2026_01_08_040736_modify_booking_requests_table.php
+++ database/migrations/2026_01_08_040736_modify_booking_requests_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         //
         Schema::table("booking_requests", function (Blueprint $table) {
-            
+            // Remove Unused fields
+            $table->dropColumn(['start_time', 'end_time']);
+
+            // Add wedding related fields
+            $table->text('opening_songs')->nullable()->after('event_date');
+            $table->text('special_moments')->nullable()->after('opening_songs');
+
         });
     }
 
@@ -23,5 +29,10 @@ return new class extends Migration
     public function down(): void
     {
         //
+        Schema::table('booking_requests', function (Blueprint $table) {
+            $table->dropColumn(['opening_songs', 'special_moments']);
+            $table->time('start_time')->nullable()->after('event_date');
+            $table->time('end_time')->nullable()->after('start_time');
+        });
     }
 };
